4440a60822dcb2723f966d8958a1dce069ee953a
solihin150109 <uhuywawak@gmail.com>
2025-06-05 03:32:02 +0700

    Update settings.py

--- wqi_project/settings.py
+++ wqi_project/settings.py
@@ -55,11 +55,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wqi_project.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-    #{
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
 AUTH_PASSWORD_VALIDATORS = [
